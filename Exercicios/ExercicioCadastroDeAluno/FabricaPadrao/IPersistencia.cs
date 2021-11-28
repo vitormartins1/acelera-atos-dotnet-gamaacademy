df0820d57d52613fbac43409f896b4ba46e2468f
Vitor Martins <vitormartins.one@gmail.com>
2021-11-28 20:36:57 -0300

    inicio da implementacao do exercicio factory pattern

---
+++ Exercicios/ExercicioCadastroDeAluno/FabricaPadrao/IPersistencia.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FabricaPadrao {
+    interface IPersistencia {
+        EntidadeBase CadastrarEntidade(EntidadeBase eb);
+        List<EntidadeBase> ListarEntidade();
+    }
+}
