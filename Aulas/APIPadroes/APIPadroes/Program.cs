afc74323eddadc9ca5ee49fc165f746c3dc70a81
Vitor Martins <vitormartins.one@gmail.com>
2021-12-16 22:26:18 -0300

    ajustes na estrutura de pastas

--- Aulas/APIPadroes/APIPadroes/Program.cs
+++ Aulas/APIPadroes/APIPadroes/Program.cs
@@ -1,6 +1,23 @@
+using APIPadroes.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<TESTEContext>();
+
+builder.Services
+    .AddControllers()
+    .ConfigureApiBehaviorOptions(options =>
+    {
+        options.SuppressModelStateInvalidFilter = true; //Não valida mais automaticamente o model state
+    });
+
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.MapControllers();
 
 app.Run();
---
+++ Aulas/Exemplos Orientacao Objetos/APIPadroes/APIPadroes/Controllers/AlunoController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APIPadroes.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AlunoController : ControllerBase
+    {
+    }
+}
